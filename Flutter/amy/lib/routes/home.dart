fd74b88bef462dd8122a3566071e8706db36a8d8
Pranav <pranav.bhawan@gmail.com>
2022-01-31 17:25:22 +0530

    Added loader

--- Flutter/amy/lib/routes/home.dart
+++ Flutter/amy/lib/routes/home.dart
@@ -2,6 +2,7 @@
 
 import 'package:amy/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,13 +11,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        alignment: Alignment(1, 1),
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/bg1.png"),
             fit: BoxFit.cover,
           ),
         ),
-        child: null /* add child content here */,
+        child: Column(
+          children: const [SpinKitCircle(color: apricotWhite, size: 50.0)],
+        ) /* add child content here */,
       ),
     );
   }
