e7276bc31cb2fec6227ccb1fb68910f92e820900
Madhav Agarwal <madhav@Madhavs-MacBook-Air.local>
2022-02-03 00:29:15 +0530

    Donate page structure done

--- Flutter/amy/lib/routes/user/udonate.dart
+++ Flutter/amy/lib/routes/user/udonate.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../constants.dart';
+
 class UDonateScreen extends StatelessWidget {
   const UDonateScreen({Key? key}) : super(key: key);
+
+  get bottomAppBarContents => null;
 
   @override
   Widget build(BuildContext context) {
@@ -9,17 +13,87 @@ class UDonateScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('UDonate Screen'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          // Within the UDonateScreen widget
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pushNamed(context, '/');
-          },
-          child: const Text('Go back!'),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg1.png"),
+            fit: BoxFit.cover,
+          ),
         ),
+        child: Column(
+         //  crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:  [
+            ElevatedButton(
+            // Within the UDonateScreen widget
+            onPressed: () {
+          // Navigate back to the first screen by popping the current route
+          // off the stack.
+          Navigator.pushNamed(context, '/');
+    },
+      child: const Text('Go back!'),
+    ),
+            StyledButtonMonterrsat(
+              // Within the UDonateScreen widget
+              onPressed: () {
+                // Navigate back to the first screen by popping the current route
+                // off the stack.
+                Navigator.pushNamed(context, '/');
+              }, text: 'Donate!',
+
+            ),
+
+            Container(
+              //  alignment: Alignment.bottomCenter,
+              height: 50.0,
+              width: double.infinity,
+
+            ),
+            Container(
+            // //  alignment: Alignment.bottomCenter,
+             height: 50.0,
+            //    width: double.infinity,
+              color: Color(0xffC0D8C0),
+                child: Row(
+
+                  //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:  [
+                    ElevatedButton(
+                      // Within the UDonateScreen widget
+                      onPressed: () {
+                        // Navigate back to the first screen by popping the current route
+                        // off the stack.
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: const Text('Home'),
+                    ),
+                    ElevatedButton(
+                      // Within the UDonateScreen widget
+                      onPressed: () {
+                        // Navigate back to the first screen by popping the current route
+                        // off the stack.
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: const Text('Test Button 2'),
+                    ),
+                    ElevatedButton(
+                      // Within the UDonateScreen widget
+                      onPressed: () {
+                        // Navigate back to the first screen by popping the current route
+                        // off the stack.
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: const Text('Test Button 3'),
+                    ),
+                  ],
+                )
+            ),
+
+          ],
+
+        ) /* add child content here */,
       ),
-    );
+       );
   }
 }
