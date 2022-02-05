945ad9f4da9579adbaf319e8ee217cf37069feba
Pranav <pranav.bhawan@gmail.com>
2022-02-05 06:56:15 +0530

    Added Sihn up UI. Basic flow of pages working. Will start firebase auth now. Login first, then registration and then updation of user data on db

--- Flutter/amy/lib/routes/signup.dart
+++ Flutter/amy/lib/routes/signup.dart
@@ -1,4 +1,5 @@
 import 'package:amy/constants.dart';
+import 'package:amy/routes/user/uhome.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatelessWidget {
@@ -7,19 +8,98 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: parchmentPaper,
+      backgroundColor: lightGreen,
       appBar: AppBar(
-        title: const Text('Signup Screen'),
+        title: const Text(
+          'Sign Up Page',
+          style: TextStyle(
+              // fontWeight: FontWeight.bold,
+              color: lightGreen,
+              fontFamily: 'OpenSans',
+              fontSize: 40),
+        ),
+        backgroundColor: pineGreen,
       ),
-      body: Center(
-        child: ElevatedButton(
-          // Within the SignupScreen widget
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pushNamed(context, '/');
-          },
-          child: const Text('Go back!'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: Image.asset('assets/amy_logo.png')),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Name ',
+                ),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'snu mail id',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mess Card number',
+                ),
+              ),
+            ),
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contact number',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            StyledButtonOpenSans(
+                text: "Sign Me up",
+                onPressed: () => {Navigator.pushNamed(context, '/uhome')}),
+            const SizedBox(
+              height: 130,
+            ),
+          ],
         ),
       ),
     );
