6827a2ab998e52ae1d583fb00a62df195bef84a7
Pranav <pranav.bhawan@gmail.com>
2022-02-20 13:36:51 +0530

    Added a mock database, this will update on each user generation

--- Flutter/amy/lib/firebase_user_dealing.dart
+++ Flutter/amy/lib/firebase_user_dealing.dart
@@ -5,6 +5,9 @@ class FirebaseUser {
   static CollectionReference users =
       FirebaseFirestore.instance.collection('users');
 
+  static CollectionReference messData =
+      FirebaseFirestore.instance.collection('messData');
+
   static Future<void> addUser(String uID, String fullName, String messID,
       String phoneNumber, String email) async {
     // Call the user's CollectionReference to add a new user
@@ -16,6 +19,23 @@ class FirebaseUser {
           'messID': messID, // Stokes and Sons
           'phone': phoneNumber, // 42
           'email': email // 42
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
+  }
+
+  static Future<void> addUserMess(String uID, String fullName, String messID,
+      String phoneNumber, String email) async {
+    // Call the user's CollectionReference to add a new user
+    users
+        .doc(uID)
+        .set({
+          'uID': uID, // John Doe
+          'full_name': fullName, // John Doe
+          'messID': messID, // Stokes and Sons
+          'phone': phoneNumber, // 42
+          'email': email, // 42
+          'balance': 3800
         })
         .then((value) => print("User Added"))
         .catchError((error) => print("Failed to add user: $error"));
